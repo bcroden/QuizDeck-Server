694844da436d91c09f5083c6b2ba0e01752a3679
CadeG <chgatewo@uark.edu>
2016-02-04 22:34:54 -0600

    Redis bean

---
+++ src/main/java/com/quizdeck/RedisDB/ProdDatabaseConfiguration.java
@@ -0,0 +1,36 @@
+package com.quizdeck.RedisDB;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class ProdDatabaseConfiguration {
+
+    @Bean
+    public JedisConnectionFactory jedisConnFactory() {
+
+        try {
+            String redistogoUrl = System.getenv("REDISTOGO_URL");
+            URI redistogoUri = new URI(redistogoUrl);
+
+            JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
+
+            jedisConnFactory.setUsePool(true);
+            jedisConnFactory.setHostName(redistogoUri.getHost());
+            jedisConnFactory.setPort(redistogoUri.getPort());
+            jedisConnFactory.setTimeout(Protocol.DEFAULT_TIMEOUT);
+            jedisConnFactory.setPassword(redistogoUri.getUserInfo().split(":", 2)[1]);
+
+            return jedisConnFactory;
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
