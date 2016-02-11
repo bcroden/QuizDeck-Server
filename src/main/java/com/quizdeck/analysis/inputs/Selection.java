315037bb120e42a01d06a45c22eaa06b7f4f854d
alexv-anderson <opiemike613@yahoo.com>
2016-02-11 17:56:53 -0600

    Update analysis input classes to require equals method

--- src/main/java/com/quizdeck/analysis/inputs/Selection.java
+++ src/main/java/com/quizdeck/analysis/inputs/Selection.java
@@ -5,19 +5,14 @@ package com.quizdeck.analysis.inputs;
  *
  * @author Alex
  */
-public interface Selection {
+public abstract class Selection {
     /**
      * This is used when a string representation of the selection is needed
      * to display answers/guesses in reports.
      * @return A string representation of the selection.
      */
-    public String toDisplayString();
+    public abstract String toDisplayString();
 
-    /**
-     * Indicates if the Selection passed into the function is equivalent
-     * to this Selection.
-     * @param that The selection to which this selection is being compared
-     * @return True if the selections are equivalent, false otherwise.
-     */
-    public boolean isSameAs(Selection that);
+    public abstract boolean equals(Object o);
+    public abstract int hashCode();
 }
