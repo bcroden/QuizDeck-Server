cb78c6931941be121bdfdff6e8971a9b44fca8c9
Brandon Roden <brandoncroden@gmail.com>
2016-02-11 13:47:58 -0600

    Add global CORS filter

--- src/main/java/com/quizdeck/QuizDeckApplication.java
+++ src/main/java/com/quizdeck/QuizDeckApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.annotation.Resource;
 
@@ -28,5 +31,17 @@ public class QuizDeckApplication {
 		registration.setFilter(new AuthFilter(secretKey));
 		registration.addUrlPatterns("/rest/secure/*");
 		return registration;
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowedOrigins("*");
+			}
+		};
 	}
 }
