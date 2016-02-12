8079830ecd0c66a810bce73ff24ee5cbdd7f6444
alexv-anderson <opiemike613@yahoo.com>
2016-02-11 20:04:38 -0600

    Remove unused overloaded constructors

--- src/main/java/com/quizdeck/analysis/exceptions/AnalysisConstructionException.java
+++ src/main/java/com/quizdeck/analysis/exceptions/AnalysisConstructionException.java
@@ -6,17 +6,8 @@ package com.quizdeck.analysis.exceptions;
  * @author Alex
  */
 public class AnalysisConstructionException extends AnalysisException {
-    public AnalysisConstructionException(String message)
-    {
-        super(message);
-    }
-    public AnalysisConstructionException(Throwable throwable)
-    {
-        super(throwable);
-    }
     public AnalysisConstructionException(String message, Throwable throwable)
     {
         super(message, throwable);
     }
-
 }
