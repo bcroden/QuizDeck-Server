cd9c57e9b58b1eed4cd106ac76d36118889399e4
Brandon Roden <brandoncroden@gmail.com>
2016-02-12 16:46:02 -0600

    Only add CORS to rest urls

--- src/main/java/com/quizdeck/QuizDeckApplication.java
+++ src/main/java/com/quizdeck/QuizDeckApplication.java
@@ -39,7 +39,7 @@ public class QuizDeckApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry
-						.addMapping("/**")
+						.addMapping("/rest/**")
 						.allowedOrigins("*");
 			}
 		};
