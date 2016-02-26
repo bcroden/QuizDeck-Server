2a605cca2526e84102f947bfb3d152b39a121fe7
alexv-anderson <opiemike613@yahoo.com>
2016-02-25 23:25:29 -0600

    Add indecisiveness controller test

--- src/test/java/com/quizdeck/controllers/QuizIndecisivenessAnalysisControllerTest.java
+++ src/test/java/com/quizdeck/controllers/QuizIndecisivenessAnalysisControllerTest.java
@@ -1,30 +1,25 @@
 package com.quizdeck.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.quizdeck.analysis.inputs.Question;
 import com.quizdeck.analysis.outputs.QuizAnalysisData;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Test the quiz accuracy analysis controller
+ * Test the quiz indecisiveness controller
  *
  * @author Alex
  */
-public class QuizAccuracyAnalysisControllerTest extends AbstractQuizAnalysisControllerTest {
-
+public class QuizIndecisivenessAnalysisControllerTest extends AbstractQuizAnalysisControllerTest {
     @Test
-    public void testQuizAccuracyResults() throws Exception {
-        String result = mockMvc.perform(post("/rest/secure/analysis/accuracy/").content(this.json(accuracyInput))
+    public void testIndecisivenessResults() throws Exception {
+        String result = mockMvc.perform(post("/rest/secure/analysis/indecisiveness/").content(this.json(accuracyInput))
                                                 .contentType(MediaType.APPLICATION_JSON)
                                         ).andExpect(status().is2xxSuccessful())
                                         .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
