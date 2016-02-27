b644079f852d6e23ce3a90a8273af3dc16100c41
CadeG <chgatewo@uark.edu>
2016-02-27 16:42:26 -0600

    Changed test mock to get request

--- src/test/java/com/quizdeck/controllers/QuizAccuracyAnalysisControllerTest.java
+++ src/test/java/com/quizdeck/controllers/QuizAccuracyAnalysisControllerTest.java
@@ -1,17 +1,13 @@
 package com.quizdeck.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.quizdeck.analysis.inputs.Question;
 import com.quizdeck.analysis.outputs.QuizAnalysisData;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,7 +20,7 @@ public class QuizAccuracyAnalysisControllerTest extends AbstractQuizAnalysisCont
 
     @Test
     public void testQuizAccuracyResults() throws Exception {
-        String result = mockMvc.perform(post("/rest/secure/analysis/accuracy/").content(this.json(accuracyInput))
+        String result = mockMvc.perform(get("/rest/secure/analysis/accuracy/").content(this.json(accuracyInput))
                                                 .contentType(MediaType.APPLICATION_JSON)
                                         ).andExpect(status().is2xxSuccessful())
                                         .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
