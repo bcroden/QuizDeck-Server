7df41e14890d2cca5c8cdc0ef26b458aa6e76e52
CadeG <chgatewo@uark.edu>
2016-02-27 21:28:23 -0600

    Cleaned up unneeded objects, created endpoints for requests related to user quizzes

---
+++ src/main/java/com/quizdeck/model/inputs/OwnerTitleQuizSearchInput.java
@@ -0,0 +1,17 @@
+package com.quizdeck.model.inputs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Cade on 2/27/2016.
+ */
+
+@Getter
+@Setter
+public class OwnerTitleQuizSearchInput {
+
+    private String owner;
+    private String title;
+
+}
