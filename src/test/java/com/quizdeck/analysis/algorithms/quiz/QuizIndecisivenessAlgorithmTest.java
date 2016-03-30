ee08b496801c1c0f2148924e5917a9e34fa3c32c
alexv-anderson <opiemike613@yahoo.com>
2016-03-30 17:46:30 -0500

    Refactor analysis test package

--- src/test/java/com/quizdeck/analysis/algorithms/quiz/QuizIndecisivenessAlgorithmTest.java
+++ src/test/java/com/quizdeck/analysis/algorithms/quiz/QuizIndecisivenessAlgorithmTest.java
@@ -1,4 +1,4 @@
-package com.quizdeck.analysis.algorithms;
+package com.quizdeck.analysis.algorithms.quiz;
 
 import com.quizdeck.analysis.*;
 import com.quizdeck.analysis.exceptions.AnalysisException;
